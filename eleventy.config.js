f098c287c7538e4690483948b2b8da2d96879372
HERA <everydaymay21@gmail.com>
2025-06-17 17:27:48 +0900

    노 지 킬을 환영해요

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles.css");
+  eleventyConfig.addPassthroughCopy("src/.nojekyll");
   return {
     dir: {
       input: "src",
